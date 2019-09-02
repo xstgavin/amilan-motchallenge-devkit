54e98e755cdd205b4bfa0d76372b2f0807d9275f
i-xiaoshengtao <i-xiaoshengtao@360.cn>
2019-09-02 15:06:24 +0800

    tested homebrain_short list with gt detection

--- eval_trackor.m
+++ eval_trackor.m
@@ -1,5 +1,8 @@
-benchmarkDir = '../data/MOT17Det/train/';
+%benchmarkDir = '../data/MOT17Det/train/';
 %[allMets, metsBenchmark] =evaluateTracking('c9-train.txt', 'res/MOT17_Tracktor/train_FRCNN/', benchmarkDir, 'MOT17');
 %[allMets, metsBenchmark] = evaluateTracking('c5-train.txt', 'res/MOT16/data/', benchmarkGtDir, 'MOT16');
-benchmarkDir = '../data/MOT17Det/test/';
-[allMets, metsBenchmark] =evaluateTracking('c9-train.txt', 'res/MOT17_Tracktor/test_FRCNN/', benchmarkDir, 'MOT17');
+%benchmarkDir = '../data/MOT17Det/test/';
+%[allMets, metsBenchmark] =evaluateTracking('c9-train.txt', 'res/MOT17_Tracktor/test_FRCNN/', benchmarkDir, 'MOT17');
+
+benchmarkDir = '../data/homebrain/homebrain_short/';
+[allMets, metsBenchmark] =evaluateTracking('homebrain_short.txt', 'res/homebrain/homebrain_short/', benchmarkDir, 'homebrain');
